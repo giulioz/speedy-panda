1cb7382fc3e23ff4b7039be6dd1227c5ec546c87
Giulio Zausa <mail.zausa.giulio@gmail.com>
2020-09-26 13:08:02 +0200

    Transaction read code

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,40 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+using transactions = std::vector<std::vector<int>>;
+
+transactions readTransactions(std::string path) {
+  transactions result;
+
+  std::ifstream infile(path);
+  std::string line;
+  while (std::getline(infile, line)) {
+    std::stringstream ss(line);
+    int val;
+    std::vector<int> row;
+
+    while (ss >> val) {
+      row.push_back(val);
+      if (ss.peek() == ' ') ss.ignore();
+    }
+
+    result.push_back(row);
+  }
+
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  transactions tr = readTransactions(argv[1]);
+  for (auto &&t : tr) {
+    for (auto &&c : t) {
+      std::cout << c << " ";
+    }
+    std::cout << std::endl;
+  }
+
   return 0;
 }
