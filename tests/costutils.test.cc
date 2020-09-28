158ff5a6b37feb2e6611e26cc35889d834e85708
Giulio Zausa <mail.zausa.giulio@gmail.com>
2020-09-28 13:12:34 +0200

    Tests for calcPatternsComplexity

--- tests/costutils.test.cc
+++ tests/costutils.test.cc
@@ -1,13 +1,39 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../src/costutils.h"
 #include "doctest.h"
 
-int factorial(int number) {
-  return 0;
-}
+TEST_CASE("calcPatternsComplexity") {
+  PatternList patterns;
+  CHECK(calcPatternsComplexity(patterns) == 0);
 
-TEST_CASE("testing the factorial function") {
-  CHECK(factorial(1) == 1);
-  CHECK(factorial(2) == 2);
-  CHECK(factorial(3) == 6);
-  CHECK(factorial(10) == 3628800);
+  Pattern p1;
+  patterns.push_back(p1);
+  CHECK(calcPatternsComplexity(patterns) == 0);
+
+  Pattern p2;
+  p2.itemIds.insert(5);
+  p2.itemIds.insert(5);
+  p2.itemIds.insert(3);
+  p2.transactionIds.insert(5);
+  p2.transactionIds.insert(5);
+  p2.transactionIds.insert(3);
+  p2.transactionIds.insert(8);
+  patterns.push_back(p2);
+  CHECK(calcPatternsComplexity(patterns) == 5);
+
+  Pattern p3;
+  p3.itemIds.insert(1);
+  p3.itemIds.insert(2);
+  p3.transactionIds.insert(3);
+  patterns.push_back(p3);
+  CHECK(calcPatternsComplexity(patterns) == 8);
+
+  Pattern p4;
+  p4.itemIds.insert(1);
+  p4.itemIds.insert(2);
+  p4.itemIds.insert(5);
+  p4.transactionIds.insert(3);
+  p4.transactionIds.insert(8);
+  patterns.push_back(p4);
+  CHECK(calcPatternsComplexity(patterns) == 13);
 }
