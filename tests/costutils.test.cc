3dc6cc4af4c2733d4d69671cf5667ca3299eab2c
Giulio Zausa <mail.zausa.giulio@gmail.com>
2020-09-28 13:31:22 +0200

    Tests for calcNoise

--- tests/costutils.test.cc
+++ tests/costutils.test.cc
@@ -36,3 +36,56 @@ TEST_CASE("calcPatternsComplexity") {
   patterns.push_back(p4);
   CHECK(calcPatternsComplexity(patterns) == 13);
 }
+
+TEST_CASE("calcNoise") {
+  TransactionList dataset;
+  PatternList patterns;
+  CHECK(calcNoise(patterns, dataset) == 0);
+
+  dataset.push_back({0, 1, 2});
+  CHECK(calcNoise(patterns, dataset) == 3);
+  dataset.push_back({0, 1, 5});
+  CHECK(calcNoise(patterns, dataset) == 6);
+
+  Pattern p1;
+  p1.itemIds = {0, 1, 2};
+  p1.transactionIds = {0, 1};
+  patterns.push_back(p1);
+  CHECK(calcNoise(patterns, dataset) == 2);
+
+  dataset.push_back({0, 1, 2});
+  CHECK(calcNoise(patterns, dataset) == 5);
+
+  dataset.push_back({7, 8, 9});
+  dataset.push_back({7, 8, 9});
+  dataset.push_back({5, 10, 11});
+  dataset.push_back({8, 9, 7});
+  dataset.push_back({7, 8, 9, 10});
+  CHECK(calcNoise(patterns, dataset) == 21);
+
+  Pattern p2;
+  p2.itemIds = {9, 8, 7};
+  p2.transactionIds = {3, 4, 6, 7};
+  patterns.push_back(p2);
+  CHECK(calcNoise(patterns, dataset) == 9);
+
+  SUBCASE("Overlapping patterns") {
+    dataset.push_back({3, 4, 5});
+    dataset.push_back({3, 4, 5});
+    dataset.push_back({3, 4, 5, 6, 7, 8});
+    dataset.push_back({3, 4, 5, 6, 7, 8, 9});
+    CHECK(calcNoise(patterns, dataset) == 28);
+
+    Pattern p3;
+    p3.itemIds = {3, 4, 5};
+    p3.transactionIds = {8, 9, 10, 11};
+    patterns.push_back(p3);
+    CHECK(calcNoise(patterns, dataset) == 16);
+
+    Pattern p4;
+    p4.itemIds = {6, 7, 8};
+    p4.transactionIds = {10, 11};
+    patterns.push_back(p4);
+    CHECK(calcNoise(patterns, dataset) == 10);
+  }
+}
